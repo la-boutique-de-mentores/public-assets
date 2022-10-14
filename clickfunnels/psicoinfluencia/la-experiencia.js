ae6b328178546af32b245d0424d23e5e8506abde
Helder Manuel Torres Vieira <helder.torres@innki.tech>
2022-10-14 11:34:31 +0200

    feat: 🚀 added clickfunnels scripts

---
+++ clickfunnels/psicoinfluencia/la-experiencia.js
@@ -0,0 +1,58 @@
+// Settings
+const refCodeMappingTable = {
+  "helder-torres": {},
+  "vikrampal-singh": {},
+  "prem-sukhdev": {},
+  "cathaysa-melian": {},
+  "adrian-sobrino": {},
+  "pedro-sobrino": {},
+  "noelia-martin": {},
+  "marisol-lopez": {},
+  "alejandra-esteban": {},
+  "carlos-avila": {},
+  "rocio-gimenez": {},
+  "carlos-martin": {},
+  "sheila-diaz": {},
+  "harrison-del-castillo": {},
+  "ade-ramirez": {},
+  "radha-chand": {},
+  "diana-de-maria": {},
+  "inma-martin": {},
+  "pilar-navarro": {},
+  "laura-morales": {},
+  "jorge-juan": {},
+  "carmen-caballero": {},
+  "yuri-arguinzones": {},
+  "ruben-fornell": {},
+  "idoia-izaguirre": {},
+  "manuel-mba": {},
+  "bo-rademakers": {},
+  "sara-verissimo": {},
+  "javier-rodriguez": {},
+  "elena-vazquez": {},
+  "sheila-de-leon": {},
+  "juan-gasca": {},
+  "fernando-hernandez": {},
+  "samuel-guerrero": {},
+  "flory-ramirez": {},
+  "juanma-escalera": {},
+  "jose-manuel-arroyo": {},
+  "sandra-logan": {},
+  "carmen-bargues": {},
+  "nuria-urcelay": {},
+  "celia-vergara": {},
+  "maria-celeste": {},
+  "montse-valverde": {},
+  "ilaria-gonzales": {}
+};
+
+const refCodeParam          = "ref_code_la_experiencia";
+const queryParams           = new URLSearchParams(window.location.search);
+const refCodeValue          = queryParams.get(refCodeParam) ?? localStorage.getItem(refCodeParam.split("_").join("-"));
+const refCodeValueValid     = validateRefCode(refCodeMappingTable, refCodeValue);
+
+// Store Ref Code
+storeRefCode(refCodeValueValid, refCodeParam.split("_").join("-"), refCodeValue);
+
+// Remove Query Params
+removeQueryParams();
---
+++ clickfunnels/psicoinfluencia/taller-online-codice-checkout.js
@@ -0,0 +1,36 @@
+// Settings
+const couponCodeParam         = "off";
+const queryParams             = new URLSearchParams(window.location.search);
+const couponCodeValue         = queryParams.get(couponCodeParam);
+const payProcessorParam       = "pay_processor";
+const payFromSpainParam       = "pay_from_spain";
+const payTaxExemptionParam    = "pay_tax_exemption";
+const payProcessorSelector    = document.querySelector(`[data-custom-type="${payProcessorParam}"]`);
+const payFromSpainSelector    = document.querySelector(`[data-custom-type="${payFromSpainParam}"]`);
+const payTaxExemptionSelector = document.querySelector(`[data-custom-type="${payTaxExemptionParam}"]`);
+const thrivecartObj           = {
+  account: "laboutiquedementores",
+  queryParams: { "coupon_cache": false },
+  checkouts: {
+    stripe: {
+      t: {id: 6, slug: "tc-laboutiquedementores-6-1V533N"},
+      n: {id: 7, slug: "tc-laboutiquedementores-7-ORM8IE"}
+    },
+    paypal: {
+      t: {id: 16, slug: "tc-laboutiquedementores-16-KBPEVW"},
+      n: {id: 17, slug: "tc-laboutiquedementores-17-2KO8LS"}
+    }
+  }
+}
+
+// Add Thrivecart Params
+if (couponCodeValue !== null) thrivecartObj.queryParams["coupon"] = couponCodeValue;
+
+// Remove Query Params
+removeQueryParams();
+
+// Add Event Listeners
+payProcessorSelector.addEventListener("change", () => toggleThrivecartCheckout(thrivecartObj, payProcessorParam, payFromSpainParam, payTaxExemptionParam));
+payTaxExemptionSelector.addEventListener("change", () => toggleThrivecartCheckout(thrivecartObj, payProcessorParam, payFromSpainParam, payTaxExemptionParam));
+payFromSpainSelector.addEventListener("change", () => toggleThrivecartCheckout(thrivecartObj, payProcessorParam, payFromSpainParam, payTaxExemptionParam));
+window.addEventListener("load", () => toggleThrivecartCheckout(thrivecartObj, payProcessorParam, payFromSpainParam, payTaxExemptionParam));
