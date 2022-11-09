2b523635f090b45a39e3b2e5aff621a08e3b0a83
Helder Manuel Torres Vieira <helder.torres@innki.tech>
2022-11-09 03:17:44 +0100

    style: :memo: added copyright header

--- scripts/clickfunnels/psicoinfluencia/la-experiencia-checkout.js
+++ scripts/clickfunnels/psicoinfluencia/la-experiencia-checkout.js
@@ -1,6 +1,13 @@
 "use strict";
 
 var _queryParams$get;
+// ---------------------------------------------------------------- //
+// Copyright: Innki Tech 2022. All Rights Reserved                  //
+// Mantainer: Helder Torres <helder.torres@innki.tech>              //
+// This file is licensed under the MPL 2.0 license                  //
+// License text available at https://www.mozilla.org/en-US/MPL/2.0  //
+// ---------------------------------------------------------------- //
+
 // Settings
 var refCodeMappingTable = {
   "helder-torres": {},
