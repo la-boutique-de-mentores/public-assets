9259c6e4581cfb1fdc9db917e5ae11bf9304b01f
Helder Manuel Torres Vieira <helder.torres@innki.tech>
2022-11-09 01:38:13 +0100

    chore: 🔧 added thrivecart script

---
+++ scripts/thrivecart/thrivecart.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var thrivecartQueryParams = new URLSearchParams(window.location.search);
+var thrivecartCustomerAddressCountry = thrivecartQueryParams.get("passthrough[customer_address_country]");
+var thrivecartCustomerAddressState = thrivecartQueryParams.get("passthrough[customer_address_state]");
+setTimeout(function () {
+  if (thrivecartCustomerAddressCountry != null) {
+    thrivecartCustomerAddressCountrySelector = document.querySelector("#field-customer-address-country");
+    thrivecartCustomerAddressCountrySelector.disabled = true;
+  }
+  if (thrivecartCustomerAddressState != null) {
+    thrivecartCustomerAddressStateSelector = document.querySelector("#field-customer-address-state");
+    thrivecartCustomerAddressStateSelector.value = thrivecartCustomerAddressState;
+    thrivecartCustomerAddressStateSelector.disabled = true;
+  }
+}, 2000);
